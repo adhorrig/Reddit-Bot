67476db01efa2505908a1f192bc95dc0a8efd1ad
ADHORRIG <adhorrig@gmail.com>
2015-10-03 18:04:59 +0100

    First commit
    Reddit bot to reply to comments that misspelled definitely. Not working
    as of yet.

---
+++ bot.py
@@ -0,0 +1,28 @@
+import praw
+import time
+
+r = praw.Reddit(user_agent = "Simple replybot by Adam")
+print("Logging in...")
+r.login("DefinitelyCorrect", "DefinitelyCorrect") #Possible error here.
+
+words_to_match = ['definately', 'defiantly', 'definantly', 'definetly', 'definatley']
+cache = []
+
+def run_bot():
+	print("Grabbing subreddit...")
+	subreddit = r.get_subreddit("test")
+	print("Grabbing comments...")
+	comments = subreddit.get_comments(limit=35)
+	for comment in comments:
+		comment_text = comment.body.lower()
+		isMatch = any(string in comment_text for string in words_to_match)
+		if comment.id not in cache and isMatch:
+			print("Match found! Comment ID: " +comment.id)
+			comment.reply('Did you mean to spell "definitely?"') #Possible error here.
+			print("Reply succesful!")
+			cache.appent(comment.id)
+		print("Comments loop finished.")
+			
+while True:
+	run_bot()
+	time.sleep(10)
